51c14c5b9900eee769f46fd11f2d17be5b9d91b9
SuperDev314 <sdfdfg314@gmail.com>
2017-06-13 14:16:50 +0530

    updated config

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ module.exports = exports = {
     port: 3000,
     // Contentstack Config
     contentstack: {
-        api_key: 'blt2f8208e2d15beeb0',
-        access_token: 'blt108b711b0301c616'
+        api_key: '',
+        access_token: ''
     }
 };
