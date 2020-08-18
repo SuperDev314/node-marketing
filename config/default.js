32d572fdac7b73017d2d5d00e96964ffafb67ecb
SuperDev314 <sdfdfg314@gmail.com>
2020-08-18 18:06:18 +0530

    first build

--- config/default.js
+++ config/default.js
@@ -2,8 +2,8 @@ module.exports = exports = {
     port: 3000,
     // Contentstack Config
     contentstack: {
-        api_key: 'Your ApiKey',
-        delivery_token: 'Your Delivery Token',
-        environment: 'Your Publishing Environment'
+        api_key: 'bltf0aeb46e292dbc44',
+        delivery_token: 'cs8284c7ee635fbc77d29cbeb0',
+        environment: 'development'
     }
 };
